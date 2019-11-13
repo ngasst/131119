93c1171d28ea5bfb70ede97b6ffb75358eaaff03
Gaston Ndanyuzwe <ndanyuzwe@hotmail.com>
2019-11-13 20:56:32 +0100

    add possibility to remove counter

--- src/components/counter.js
+++ src/components/counter.js
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import React from "react";
+import { connect } from "react-redux";
 const Wrapper = styled.div`
     display: flex;
     width: 100%;
@@ -19,17 +20,45 @@ const Wrapper = styled.div`
     }
 
     .increment {
-        background-color: darkred;
+        background-color: deepskyblue;
         color: white;
+    }
+
+    .remove {
+        background-color: red;
     }
 `;
 
-export function Counter(props) {
+function Component(props) {
     return (
         <Wrapper>
             <button className="decrement">-</button>
             <div className="count">{props.counter.value}</div>
             <button className="increment">+</button>
+            <button
+                className="remove"
+                onClick={() => props.removeCounter(props.counter.id)}
+            >
+                x
+            </button>
         </Wrapper>
     );
 }
+
+const mapStateToProps = state => {
+    return {};
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        removeCounter(id) {
+            const action = {
+                type: "REMOVE_COUNTER",
+                payload: id
+            };
+
+            dispatch(action);
+        }
+    };
+};
+export const Counter = connect(mapStateToProps, mapDispatchToProps)(Component);
